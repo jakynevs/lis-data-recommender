c77f0ee75f34ad71161cfb608bf499186d9970f3
jakynevs <johnnevilleis@hotmail.co.uk>
2024-03-12 10:28:11 +0100

    Added default value for the dropdown options

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,4 +1,3 @@
-    // Dropdown.js
 import React from 'react';
 import { DropdownContainer, Select } from './DropdownStyles';
 
@@ -6,6 +5,7 @@ const Dropdown = ({ options, defaultValue, onChange }) => {
   return (
     <DropdownContainer>
       <Select defaultValue={defaultValue} onChange={onChange}>
+      <option value="" disabled selected>Choose an option</option>
         {options.map((option, index) => (
           <option key={index} value={option.value}>
             {option.label}
